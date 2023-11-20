fc90a2600249f4ec9c02ee2263b34be18526692b
aaaaaa27 <650112418061@bru.ac.th>
2023-11-20 11:54:46 +0700

    lab3

--- lab2/html/app.js
+++ lab2/html/app.js
@@ -1,19 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p id="title"> Hello Docker !!</p>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-</body>
-</html>
-
-<script>
     var num = 10;
     age = 20;
     const year = 1983;
@@ -48,4 +33,6 @@
         .addClass('red')
     });
 
-</script>
+
+
+
